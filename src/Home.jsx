c9361a7ab60ac39427a72a88368cbd22d348eceb
ipreferlee <lrochoada@gbox.ncf.edu.ph>
2024-11-21 17:17:23 +0800

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
           <Navbar.Toggle aria-controls="navbarNav" />
           <Navbar.Collapse id="navbarNav" className="justify-content-center">
             <Nav className="mx-auto">
-              <Nav.Link as={Link} to="/home">Home</Nav.Link>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/about">About</Nav.Link>
               <Nav.Link as={Link} to="/education">Education</Nav.Link>
               <Nav.Link as={Link} to="/hobbies">Hobbies</Nav.Link>
