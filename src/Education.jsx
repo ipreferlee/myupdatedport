a8fa9a17ae035732f7f93d5983967183f58b0c3b
ipreferlee <lrochoada@gbox.ncf.edu.ph>
2024-11-21 17:37:44 +0800

    Update Education.jsx

--- src/Education.jsx
+++ src/Education.jsx
@@ -18,7 +18,7 @@ const Education = () => {
           <Navbar.Toggle aria-controls="navbarNav" />
           <Navbar.Collapse id="navbarNav" className="justify-content-center">
             <Nav className="mx-auto">
-              <Nav.Link as={Link} to="/home">Home</Nav.Link>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/about">About</Nav.Link>
               <Nav.Link as={Link} to="/education">Education</Nav.Link>
               <Nav.Link as={Link} to="/hobbies">Hobbies</Nav.Link>
