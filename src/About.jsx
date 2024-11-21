680d8e98688cd5ef168fe7b6d4e74da4a95396db
ipreferlee <lrochoada@gbox.ncf.edu.ph>
2024-11-22 00:11:47 +0800

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ import myImage from './assets/Person-Assets.png';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { Link } from 'react-router-dom';
 
 
 
