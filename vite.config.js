d4a1711d06b932ba81d871c578f8ff71bd8b0fae
ipreferlee <lrochoada@gbox.ncf.edu.ph>
2024-11-21 16:46:58 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
-     // Use a port that's not in use
-  },
-}
+export default defineConfig({
+  plugins: [react()],
+})
