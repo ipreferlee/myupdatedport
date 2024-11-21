9dc65ca60a0f024e75b14badc0fbdf47b98edac8
ipreferlee <lrochoada@gbox.ncf.edu.ph>
2024-11-21 16:44:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default {
-  server: {
-    host: '0.0.0.0', // Allow external access
-    port: 3000,       // Use a port that's not in use
+     // Use a port that's not in use
   },
 }
